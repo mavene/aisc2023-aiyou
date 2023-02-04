0ff70f0b65d70d7481d838d968a2e17b07552f60
mavene <dianmaisara@gmail.com>
2023-02-04 14:45:40 +0800

    really fixed dependencies

--- app/models.py
+++ app/models.py
@@ -1,27 +1,6 @@
-from app import db, application
+from app import db, application, webscrape
 import pandas as pd
 import os
-
-# from app import login
-# from flask_login import UserMixin
-# from werkzeug.security import generate_password_hash, check_password_hash
-
-# class User(UserMixin, db.Model):
-# 	__tablename__ = 'user'
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	username = db.Column(db.String(64), index=True, unique=True)
-# 	password_hash = db.Column(db.String(128))
-# 	review = db.relationship('Review', backref='from_user', 
-# 								lazy='dynamic')
-
-# 	def set_password(self, password):
-# 		self.password_hash = generate_password_hash(password)
-
-# 	def check_password(self, password):
-# 		return check_password_hash(self.password_hash, password)
-
-# 	def __repr__(self):
-# 		return f'<User {self.username:}>'
 
 # Parent
 class Entity(db.Model):
@@ -47,7 +26,7 @@ class Review(db.Model):
     
 with application.app_context():
     db.create_all()
-    
+
     csv_files = ["entity_data.csv", "review_data.csv"]
     for csv in csv_files:
         filename = os.path.dirname(os.path.abspath(__file__)) + "\\" + csv
